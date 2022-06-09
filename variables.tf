806e79016ba416d06601ed0e9a10d5d945790164
paulieguk <paul.gregory@skillable.com>
2022-06-09 13:59:04 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@
 #}
 
 variable "storage_account_name" {
-  default       = "[saname]"
+  default       = "saname"
   description   = "Prefix of the storage acount name with the rest of the FQDN for storage."
 }
 
