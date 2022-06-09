ae95da549b90d64ca8f6dcdbb0fbccc7e801252a
paulieguk <paul.gregory@skillable.com>
2022-06-09 12:47:44 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
-variable "resource_group_name" {
-  default       = "<rghere>"
-  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
-}
+#variable "resource_group_name" {
+#  default       = "<rghere>"
+#  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+#}
 
 variable "storage_account_name" {
   default       = "<sahere>"
   description   = "Prefix of the storage acount name with the rest of the FQDN for storage."
 }
 
-variable "resource_group_location" {
-  default       = "eastus"
-  description   = "Location of the resource group."
+#variable "resource_group_location" {
+#  default       = "eastus"
+#  description   = "Location of the resource group."
 }
