98ffa092c33e9a43982cd86e08ef24d105659f1a
paulieguk <paul.gregory@skillable.com>
2022-06-09 15:07:38 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,13 +2,6 @@ terraform {
 
   required_version = ">=0.12"
 
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-}
 
 provider "azurerm" {
   skip_provider_registration = "true"
